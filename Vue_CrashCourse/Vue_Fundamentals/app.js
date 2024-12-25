540e8044c86b846a70a727a65137b8e1154c77aa
Tczerwien <tczerwien@hawk.iit.edu>
2024-12-24 23:12:41 -0600

    1hour 10min into video. Today I covered Directives, two-way data binding, binding attributes, outputting Raw, listening to events, and passing data with events.

--- Vue_CrashCourse/Vue_Fundamentals/app.js
+++ Vue_CrashCourse/Vue_Fundamentals/app.js
@@ -1,3 +1,4 @@
+//Its most common to have one instace of Vue open
 const vm = Vue.createApp({
     data(){
         return {
@@ -8,16 +9,41 @@ const vm = Vue.createApp({
                 }, 
                 person_2: {
                     firstName: 'Thomas'
+                },
+                person_3: {
+                    firstName: 'Daniel'
                 }
-            }
+            },
+            url: 'https://www.google.com',
+            rawURL: '<a href="https://x.com/home" target="_blank">X</a>',
+            ages: {
+                person_1: { age: 20 },
+                person_2: { age: 21 },
+                person_3: { age: 26 }
+            },
         }
     },
     methods: {
         fullName(firstName){
             return `${firstName} ${this.lastName.toUpperCase()}`
+        },
+        increment(person){
+            if (this.ages[person] && this.ages[person].age !== undefined) {
+                this.ages[person].age++;
+            }
+        },
+        decrement(person){
+            if (this.ages[person] && this.ages[person].age !== undefined) {
+                this.ages[person].age--;
+            }
+        },
+        updateLastName(event){
+            this.lastName = event.target.value;
         }
     }
 }).mount('#listNames')
+
+
 
 setTimeout(() => {
     vm.names.person_2.firstName = 'Daniel';
